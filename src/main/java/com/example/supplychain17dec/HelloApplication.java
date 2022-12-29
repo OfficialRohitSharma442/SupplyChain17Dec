eafb927dff11fc0257525072fd9aa678f3ec9072
Officialrohitsharma442 <officialrohitsharma442@gmail.com>
2022-12-29 19:15:28 +0530

    login Ui

--- src/main/java/com/example/supplychain17dec/HelloApplication.java
+++ src/main/java/com/example/supplychain17dec/HelloApplication.java
@@ -3,14 +3,34 @@ package com.example.supplychain17dec;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private GridPane loginPage(){
+        Label emailLabel = new Label("Email");
+        Label passwordLabel = new Label("Password");
+
+        TextField emailTextField = new TextField();
+        PasswordField  passwordField= new PasswordField();
+        GridPane gridPane = new GridPane();
+        gridPane    .add(emailLabel,0,0);
+        gridPane.add(emailTextField,1,0);
+        gridPane.add(passwordLabel,0,1);
+         gridPane.add(passwordField,1,1);
+         return gridPane;
+    }
+
     private Pane createContent(){
+
         Pane root = new Pane();
+        root.getChildren().addAll(loginPage());
         return root;
     }
     @Override
